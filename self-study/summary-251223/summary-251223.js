b597c7435b61e8025872534857e18c6a3c4b330b
FENNEY ZHAO <jiayuzhao@uchicago.edu>
2025-12-24 03:28:29 -0600

    update

--- self-study/summary-251223/summary-251223.js
+++ self-study/summary-251223/summary-251223.js
@@ -4,3 +4,7 @@
 // chunk, bundle, hash, contenthash,loader,plugin
 
 //构建工具:snowpack,fis3,parcel,gulp,esbuild,grunt,rollup,webpack(recommended)
+
+// babel.config.js: babel 配置文件 保证语言兼容性 js编译器 看编译后结果
+//eslint: 语法纠正
+
