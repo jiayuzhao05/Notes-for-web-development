b9244e627049200c279638fd2c7cc8226cf4ad18
FENNEY ZHAO <jiayuzhao@uchicago.edu>
2025-12-23 20:54:28 -0600

    update

--- self-study/summary-251223/demos/amd/js/index.js
+++ self-study/summary-251223/demos/amd/js/index.js
@@ -1,13 +1,14 @@
 // define requirejs module using amd
 // index.js handle with index.html module needs, so we rely on jquery module
-define(['jquery'],function($){
+// rely on lodash module
+define(['jquery', 'lodash'],function($, _){
     //The code here will be executed after the jquery module is fully loaded.
     // this is what requirejs needs to handle with.
-    %('.btn').click(function(){
+    $('.btn').click(function(){
         alert('hello, what do you do today?')
     })
 })
 // lodash的throttle节流的方法限制用户访问的次数
 // 不管用户怎么点击,1秒只会触发一次
-$('.btn-seckill').click(_.throttle(function () {
-    console.log('Your grab gift request has been sent...',new Date().toTimeString())
+    $('.btn-seckill').click(_throttle(function () {
+        console.log('Your grab gift request has been sent...',new Date().toTimeString())
