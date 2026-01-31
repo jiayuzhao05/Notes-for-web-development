5ddeca04d2f9f40eeb088f39439c2150b4c0c8aa
FENNEY ZHAO <jiayuzhao@uchicago.edu>
2026-01-30 22:40:52 -0600

    update

--- lesson/lesson16/project/server.js
+++ lesson/lesson16/project/server.js
@@ -4,27 +4,26 @@ const app = express();
 const PORT = 3000;
 
 //解析application/x-www-form-urlencoded格式请求体
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 //解析JSON格式请求体
-app.use(express.json())
+app.use(express.json());
 
 app.use(express.static("public")); //提供静态文件服务
 
-app.post("/login", (request, response) => {
-  console.log(request, request.body); //获取表单数据
+app.post("/login.html", (request, response) => {
+  console.log(request.body); //获取表单数据
 
-  const{username,password}=request.body
+  const { username, password } = request.body;
   //登录逻辑
-  if(username==="admin" && password==="123456"){
-    response.send("login success")
-  }else{
-    response.send("login failed")
+  if (username === "admin" && password === "123456") {
+    response.send("login succeed");
+  } else {
+    response.send("login failed");
   }
 });
 
 app.listen(PORT, () => {
   console.log("server running on PORT 3000");
 });
-
 
 //xxx.html 实现 login 页面 http://localhost:3000/login.html
